3b663cc6db3464df74ce75bee0b15dcbead6c374
james <117844938+leejamesss@users.noreply.github.com>
2023-04-18 00:42:24 +0800

    Create 2.py

---
+++ 4.6/2.py
@@ -0,0 +1,31 @@
+# https://leetcode.cn/problems/4sum/
+#排序
+#双指针
+#枚举出所有不重复的四元组
+# 在枚举前两个数的时候就去重
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        def findNsum(l, r, target, N, result, results):
+            if r - l + 1 < N or (target < nums[l] * N) or (target > nums[r] * N):
+                return
+            if N == 2:
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s > target:
+                        r -= 1
+                    else:
+                        l += 1
+            else:
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i] != nums[i-1]):
+                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
+        
+        nums.sort()
+        results = []
+        findNsum(0, len(nums)-1, target, 4, [], results)
+        return results
