f8ced661448e0a1bd3f28bc9f2b70ff4a8427ea5
james <117844938+leejamesss@users.noreply.github.com>
2023-04-18 00:15:59 +0800

    Update 整数反转.py

--- 4.4/整数反转.py
+++ 4.4/整数反转.py
@@ -1,11 +1,12 @@
+# https://leetcode.cn/problems/reverse-integer/submissions/
 class Solution:
     def reverse(self, x: int) -> int:
         max_int, min_int = 2**31-1, -2**31
         
         res = 0
-        is_negative = x < 0
+        is_negative = x < 0                     #记录输入数字的符号
         x = abs(x)
-        while x != 0:
+        while x != 0:                           #输入数字转为正数进行反转操作        
             if res > max_int // 10 or (res == max_int // 10 and x % 10 > 7):
                 return 0
             if res < min_int // 10 or (res == min_int // 10 and x % 10 < -8):
@@ -13,7 +14,9 @@ class Solution:
             res = res * 10 + x % 10
             x //= 10
         
-        if is_negative:
+        if is_negative:                         #决定是否加上负号
             res = -res
             
         return res
+    
+
