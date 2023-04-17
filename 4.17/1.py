6756cbbc2172fea2fd307c117e45dda2e0cff361
james <117844938+leejamesss@users.noreply.github.com>
2023-04-18 00:05:25 +0800

    Update 1.py

--- 4.17/1.py
+++ 4.17/1.py
@@ -1,1 +1,28 @@
+#最长回文子串
+# https://leetcode.cn/problems/longest-palindromic-substring/
+
+#思路：中心扩散
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+
+        start = 0
+        end = 0
+        for i in range(len(s)):
+            len1 = self.expandAroundCenter(s, i, i)
+            len2 = self.expandAroundCenter(s, i, i + 1)
+            maxlen = max(len1, len2)
+            if maxlen > end - start:
+                start = i - (maxlen - 1) // 2
+                end = i + maxlen // 2
+
+        return s[start:end+1]
+
+    def expandAroundCenter(self, s: str, left: int, right: int) -> int:
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
+
 
