69abf747663c526400dddb4f705b0866b2d2dd60
james <117844938+leejamesss@users.noreply.github.com>
2023-04-18 09:41:40 +0800

    Update 1.py

--- 4.21/1.py
+++ 4.21/1.py
@@ -1,1 +1,22 @@
 
+# https://leetcode.cn/problems/wildcard-matching/
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        dp = [[False] * (n+1) for _ in range(m+1)]
+        dp[0][0] = True
+        
+        # 初始化 dp[0][j]
+        for j in range(1, n+1):
+            if p[j-1] == '*':
+                dp[0][j] = dp[0][j-1]
+        
+        # 状态转移
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if p[j-1] == s[i-1] or p[j-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-1] or dp[i-1][j]
+        
+        return dp[m][n]
