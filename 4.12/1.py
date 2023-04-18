99e2bf03a94594a28dff7d0065fe9a82bc11509b
james <117844938+leejamesss@users.noreply.github.com>
2023-04-18 08:39:26 +0800

    Update 1.py

--- 4.12/1.py
+++ 4.12/1.py
@@ -1,1 +1,24 @@
 
+#暴力匹配
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+        if len(needle) > len(haystack):
+            return -1
+        i, j = 0, 0
+        while i < len(haystack):
+            if haystack[i] == needle[j]:
+                if j == len(needle) - 1:
+                    return i - j
+                j += 1
+            else:
+                i -= j
+                j = 0
+            i += 1
+        return -1
+
+      
+      
+#KMP算法
