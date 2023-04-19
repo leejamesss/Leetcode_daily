ba19a0da19b1d0abeb53cfc82e714cf571e92072
james <117844938+leejamesss@users.noreply.github.com>
2023-04-19 08:34:01 +0800

    Update 1.py

--- 4.27/1.py
+++ 4.27/1.py
@@ -8,4 +8,10 @@ class Solution:
             #旋转90度
             matrix=list(zip(*matrix))[::-1]
         return res
- #关键在于移动的相对性
+ #关键在于移动的相对性，移动矩阵即可
+
+#补充知识点：矩阵的旋转
+#Zip实现矩阵旋转
+
+#方法1：直接调用numpy
+#方法2：zip直接转换
