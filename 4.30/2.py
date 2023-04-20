92952845fa51670b627f1ad1d0a0f4ea49380b9a
james <117844938+leejamesss@users.noreply.github.com>
2023-04-20 23:38:24 +0800

    Update 2.py

--- 4.30/2.py
+++ 4.30/2.py
@@ -1,1 +1,11 @@
-
+# https://leetcode.cn/problems/plus-one/submissions/
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        num=''
+        lst=[]
+        for i in digits:
+            num+=str(i)
+        num=str(int(num)+1)
+        for s in num:
+            lst.append(int(s))
+        return lst
