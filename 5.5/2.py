f267e5c387326a1f7ed55be77efec25bd01c100f
james <117844938+leejamesss@users.noreply.github.com>
2023-04-22 13:28:03 +0800

    Create 2.py

---
+++ 5.5/2.py
@@ -0,0 +1,20 @@
+# https://leetcode.cn/problems/sort-colors/
+# 荷兰国旗问题
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        red, white, blue = 0, 0, n - 1
+        
+        while white <= blue:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                red += 1
+                white += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
