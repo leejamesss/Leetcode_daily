9014b96b64e238cb776f9e4623b929072381213c
james <117844938+leejamesss@users.noreply.github.com>
2023-04-25 12:24:38 +0800

    Create 2.py

---
+++ 5.19/2.py
@@ -0,0 +1,12 @@
+# https://leetcode.cn/problems/same-tree/submissions/
+#首先是空节点的判断 
+#递归判断左右结点是否相同
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
