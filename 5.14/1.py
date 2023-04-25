d9f3a1f5c4465c89ace0587f716d619da4258e8f
james <117844938+leejamesss@users.noreply.github.com>
2023-04-25 12:00:36 +0800

    Update 1.py

--- 5.14/1.py
+++ 5.14/1.py
@@ -1,1 +1,10 @@
-
+# https://leetcode.cn/problems/gray-code/
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = [0]  # 初始化为 [0]
+        for i in range(n):
+            add = 1 << i  # 计算需要在哪一位加 1
+            for j in range(len(res)-1, -1, -1):
+                # 从后往前遍历，并在每个数前加上 add
+                res.append(res[j] + add)
+        return res
