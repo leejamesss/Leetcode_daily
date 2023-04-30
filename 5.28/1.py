20bda6dc365cd3c1b192a9d7ba2356fedf0a0683
james <117844938+leejamesss@users.noreply.github.com>
2023-05-01 01:11:52 +0800

    Create 1.py

---
+++ 5.28/1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows <= 0:
+            return []
+
+        res = [[1]]
+        for i in range(1, numRows):
+            row = [1] * (i + 1)
+            for j in range(1, i):
+                row[j] = res[i-1][j-1] + res[i-1][j]
+            res.append(row)
+
+        return res
