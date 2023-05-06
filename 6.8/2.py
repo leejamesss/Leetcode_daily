caf9509e9416858d3ad852fcefde132010b0f782
james <117844938+leejamesss@users.noreply.github.com>
2023-05-07 00:41:40 +0800

    Create 2.py

---
+++ 6.8/2.py
@@ -0,0 +1,8 @@
+#https://leetcode.cn/problems/single-number-ii/submissions/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ones = twos = 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
