ba0e9355a21b62dd5d40812c9ca46bc3d77823b6
james <117844938+leejamesss@users.noreply.github.com>
2023-05-09 00:01:14 +0800

    Create 2.py

---
+++ 6.16/2.py
@@ -0,0 +1,25 @@
+# https://leetcode.cn/problems/min-stack/submissions/
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.minStack[-1]:
+            self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
