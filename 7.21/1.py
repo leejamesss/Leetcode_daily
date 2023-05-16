6c1f3c57fd13e3c0eab57ff92818efabefe21974
james <117844938+leejamesss@users.noreply.github.com>
2023-05-16 13:19:41 +0800

    Create 1.py

---
+++ 7.21/1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        i, j = 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+        return False
+# https://leetcode.cn/problems/search-a-2d-matrix-ii/submissions/
