3480a1c9920f8a781dc46321f582b17bbfdea260
james <117844938+leejamesss@users.noreply.github.com>
2023-05-17 15:50:02 +0800

    Create 1.py

---
+++ 7.25/1.py
@@ -0,0 +1,11 @@
+# https://leetcode.cn/problems/missing-number/
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = n
+        for i in range(n):
+            res ^= (i ^ nums[i])
+        return res
+
