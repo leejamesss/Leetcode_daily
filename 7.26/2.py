0224ac580698bdb011d1cb20785143779533fcc5
james <117844938+leejamesss@users.noreply.github.com>
2023-05-17 16:04:13 +0800

    Create 2.py

---
+++ 7.26/2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
+# https://leetcode.cn/problems/first-bad-version/submissions/
