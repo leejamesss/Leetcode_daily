5866fa3cab99c82f264c0509f16ae86c58edd16e
james <117844938+leejamesss@users.noreply.github.com>
2023-09-13 00:16:07 +0800

    Create 2.java

---
+++ 8.27/2.java
@@ -0,0 +1,23 @@
+https://leetcode.cn/problems/kth-largest-element-in-a-stream/description/
+class KthLargest {
+    private PriorityQueue<Integer> pq;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>(k);
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (pq.size() < k) {
+            pq.offer(val);
+        } else if (pq.peek() < val) {
+            pq.poll();
+            pq.offer(val);
+        }
+        return pq.peek();
+    }
+}
