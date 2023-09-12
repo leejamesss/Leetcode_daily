f5b0a703de7a1b0c12b5c625e8e49c5d2910fc5e
james <117844938+leejamesss@users.noreply.github.com>
2023-09-13 00:15:29 +0800

    Create 1.java

---
+++ 8.27/1.java
@@ -0,0 +1,20 @@
+https://leetcode.cn/problems/count-binary-substrings/
+
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int pre = 0, cur = 1;
+        int res = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                cur++;
+            } else {
+                pre = cur;
+                cur = 1;
+            }
+            if (pre >= cur) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
